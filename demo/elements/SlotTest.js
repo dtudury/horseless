58d428bc9189fd7099fefc5dc1df7d808d2a5d23
David Tudury <david.tudury@gmail.com>
2019-03-17 22:43:21 -0700

    typo maybe

--- demo/elements/SlotTest.js
+++ demo/elements/SlotTest.js
@@ -1,14 +1,13 @@
-import lml from '../../lib'
-import { setChildren } from '../../lib/nodeCreators'
-
 /* global HTMLElement */
+import horsy from '../../lib'
+import { setChildren } from '../../lib/nodeCreators'
 
 export default class SlotTest extends HTMLElement {
   constructor () {
     super()
     this.shadow = this.attachShadow({ mode: 'open' })
 
-    this.paragraph = lml`<p>
+    this.paragraph = horsy`<p>
       This is a slot: <slot name='test'>boring text</slot><br />
       If there's something other than 'boring text" then it's working
     </p>`
@@ -17,7 +16,7 @@ export default class SlotTest extends HTMLElement {
     if (!this.config) {
       return
     }
-    const style = lml`<style>
+    const style = horsy`<style>
       p {
         color: ${this.config.color};
         background: ${this.config.background};
