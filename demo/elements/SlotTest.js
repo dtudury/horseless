1ab5b08f044a8a7e94f81caa4808a94cbdac3b75
David Tudury <david.tudury@gmail.com>
2019-03-17 22:02:39 -0700

    lit-lite-like

---
+++ demo/elements/SlotTest.js
@@ -0,0 +1,41 @@
+import lml from '../../lib'
+import { setChildren } from '../../lib/nodeCreators'
+
+/* global HTMLElement */
+
+export default class SlotTest extends HTMLElement {
+  constructor () {
+    super()
+    this.shadow = this.attachShadow({ mode: 'open' })
+
+    this.paragraph = lml`<p>
+      This is a slot: <slot name='test'>boring text</slot><br />
+      If there's something other than 'boring text" then it's working
+    </p>`
+  }
+  update () {
+    if (!this.config) {
+      return
+    }
+    const style = lml`<style>
+      p {
+        color: ${this.config.color};
+        background: ${this.config.background};
+        animation: bounce 1s ease-in-out 3;
+      }
+      @keyframes bounce {
+        0% { color: ${this.config.color}; background: ${this.config.background}}
+        50% { color: ${this.config.background}; background: ${this.config.color}}
+        100% { color: ${this.config.color}; background: ${this.config.background}}
+      }
+    </style>`
+    setChildren(this.shadow, [style, this.paragraph])
+  }
+  setAttributes (attributes) {
+    Object.assign(this, attributes)
+    this.update()
+  }
+  static get NAME () {
+    return 'slot-test-element'
+  }
+}
--- demo/test.js
+++ demo/test.js
@@ -1,8 +1,25 @@
-import lml from '../lib/lml'
+/* global customElements */
+import horsy from '../lib'
+import { setChildren } from '../lib/nodeCreators'
+import SlotTest from './elements/SlotTest'
+import CustomizedTest from './elements/CustomizedTest'
 
-console.log(lml`   asdf  ${[1, 2, 3]}asdf`)
-console.log(lml`   ${{ a: 1 }}asdf`)
-console.log(lml` <qwer>${'asdf'}</qwer>`)
-console.log(lml` <qwer a="o">asdf</qwer>`)
-console.log(lml`<asdf a=${[1, 2]}/>`)
-console.log(lml` asdf<>asdf</>`)
+const configuration = { background: 'lightgreen', color: 'darkgreen' }
+customElements.define('slot-test', SlotTest)
+customElements.define('customized-test', CustomizedTest, { extends: 'div' })
+
+setChildren(document.body, horsy`
+<svg version='1.1' baseProfile='full' width='300' height='200' viewBox='0 0 300 200' xmlns='http://www.w3.org/2000/svg'>
+  <>
+    <rect width='100%' height='100%' fill='red' />
+    <circle cx='150' cy='100' r='80' fill='green' />
+    <text x='150' y='125' font-size='60' text-anchor='middle' fill='white'>SVG</text>
+  </>
+</svg >
+
+<slot-test config=${configuration}><b slot='test'>thrilling bold text</b></slot-test>
+
+<slot-test config=${{ background: 'black', color: 'white' }}><i slot='test'>fascinating italic text</i></slot-test>
+
+<div is='customized-test' a='1' b='1'><i>This is a customized div, it console.log's itself on click</i></div>
+`)
