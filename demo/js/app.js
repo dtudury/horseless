e2fdebbe363fbfe5c4c9b46c33bc1982dacff9b7
David Tudury <david.tudury@gmail.com>
2019-04-15 22:01:07 -0700

    wip

--- demo/js/app.js
+++ demo/js/app.js
@@ -1,36 +1,28 @@
 /* global customElements */
 import { h } from '../../lib'
-import TodoList from './views/TodoList'
+import TodoItem from './views/TodoItem'
 import model from './Model'
 import { clearCompleted, addTodo } from './controller'
 
-customElements.define('todo-list', TodoList, { extends: 'ul' })
+customElements.define('todo-item', TodoItem, { extends: 'li' })
 
 const filterButtonClass = el => (model.hash === el.hash) ? 'selected' : ''
-const itemsLeft = () => model.todos.filter(todo => !todo.completed).length
-
-const main = h`<section class="main">
-  <input id="toggle-all" class="toggle-all" type="checkbox"/>
-  <label for="toggle-all">Mark all as complete</label>
-  <ul is="todo-list" class="todo-list">
-  </ul>
-</section>`
-
-const footer = h`<footer class="footer">
-  <span class="todo-count"><strong>${() => '' + itemsLeft()}</strong> item${() => itemsLeft() === 1 ? '' : 's'} left</span>
-  <ul class="filters">
-    <li>
-      <a class=${filterButtonClass}href="#/">All</a>
-    </li>
-    <li>
-      <a class=${filterButtonClass} href="#/active">Active</a>
-    </li>
-    <li>
-      <a class=${filterButtonClass} href="#/completed">Completed</a>
-    </li>
-  </ul>
-  <button class="clear-completed" onclick=${clearCompleted}>Clear completed</button>
-</footer>`
+const itemsLeft = () => model.todos.filter(todo => !todo.completed).length.toString()
+const visibleTodos = () => model.todos.filter(todo => model.hash === '#/active' ? !todo.completed : model.hash === '/#completed' ? todo.completed : true)
+let mainElements
+const showIfTodos = f => el => {
+  mainElements = mainElements || f()
+  return model.todos.length ? mainElements : []
+}
+const todosMap = new Map()
+const memoizeTodos = f => el => {
+  return visibleTodos().map(todo => {
+    if (!todosMap.has(todo)) {
+      todosMap.set(todo, f(todo))
+    }
+    return todosMap.get(todo)
+  })
+}
 
 h(document.body)`
   <section class="todoapp">
@@ -38,13 +30,32 @@ h(document.body)`
       <h1>todos</h1>
       <input class="new-todo" onchange=${e => { addTodo(e.target.value); e.target.value = '' }} placeholder="What needs to be done?" autofocus=""/>
     </header>
-  ${el => {
-    if (model.todos.length) {
-      return [main, footer]
-    } else {
-      return []
-    }
-  }}
+    ${showIfTodos(() => h`
+      <section class="main">
+        <input id="toggle-all" class="toggle-all" type="checkbox"/>
+        <label for="toggle-all">Mark all as complete</label>
+        <ul class="todo-list">
+        ${memoizeTodos(todo => h`
+          <li is="todo-item" todo=${todo}/>
+        `)}
+        </ul>
+      </section>
+      <footer class="footer">
+        <span class="todo-count"><strong>${itemsLeft}</strong> item${() => itemsLeft() === '1' ? '' : 's'} left</span>
+        <ul class="filters">
+          <li>
+            <a class=${filterButtonClass} href="#/">All</a>
+          </li>
+          <li>
+            <a class=${filterButtonClass} href="#/active">Active</a>
+          </li>
+          <li>
+            <a class=${filterButtonClass} href="#/completed">Completed</a>
+          </li>
+        </ul>
+        <button class="clear-completed" onclick=${clearCompleted}>Clear completed</button>
+      </footer>
+    `)}
   </section>
   <footer class="info">
     <p>Double-click to edit a todo</p>
