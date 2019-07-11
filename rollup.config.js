f73a3f0955a3cac55bad86da6d83214a5fbd2c48
David Tudury <david.tudury@gmail.com>
2019-07-11 00:30:45 -0700

    publish

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,11 @@
+import pkg from './package.json'
+
 export default [
   {
     input: 'lib/index.js',
-    output: {
-      name: 'horseless',
-      file: 'dist/horseless.umd.js',
-      format: 'umd'
-    }
-  },
-  {
-    input: 'lib/index.js',
     output: [
-      { file: 'dist/horseless.cjs.js', format: 'cjs' },
-      { file: 'dist/horseless.esm.js', format: 'es' }
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
     ]
   }
 ]
