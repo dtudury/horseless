6cda0781685dfd1bc54d49190b706837aa0da845
David Tudury <david.tudury@gmail.com>
2019-07-11 07:13:05 -0700

    update libraries

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,10 @@
 import pkg from './package.json'
+import uglify from 'rollup-plugin-uglify-es'
 
 export default [
   {
     input: 'lib/index.js',
+    plugins: [uglify()],
     output: [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' }
