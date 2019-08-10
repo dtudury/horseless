b79ee66d6f3553acea97156aa6a410d90dd2da3d
David Tudury <david.tudury@gmail.com>
2019-08-10 12:31:05 -0700

    maybe simplified...

--- docs/lib/nodeCreators.js
+++ docs/lib/nodeCreators.js
@@ -45,16 +45,9 @@ function _setAttributes (element, attributes, ignoreMethod = false) {
   return element
 }
 
-function _flattenAndMap (arr, f) {
-  if (!Array.isArray(arr)) {
-    return f(arr)
-  }
-  return [].concat.apply([], arr.map(v => _flattenAndMap(v, f)))
-}
-
 function _createElement (tag, attributes, descriptions, xmlns) {
   if (tag === FRAGMENT) {
-    return _flattenAndMap(descriptions, _createNode)
+    return _descriptionsToNodes(descriptions)
   } else if (typeof tag === 'function') {
     return tag(attributes, descriptions, xmlns)
   } if (typeof tag === 'string') {
@@ -66,7 +59,7 @@ function _createElement (tag, attributes, descriptions, xmlns) {
 }
 
 const _descriptionMap = new Map()
-function _createNode (description) {
+function _descriptionToNode (description) {
   if (!description) {
     return []
   } else if (description.type) {
@@ -82,34 +75,50 @@ function _createNode (description) {
   return _createTextNode(description.toString())
 }
 
+function _descriptionsToNodes (descriptions) {
+  if (!Array.isArray(descriptions)) {
+    throw new Error('descriptions must be an array')
+  }
+  const nodes = []
+  descriptions.forEach(description => {
+    if (typeof description === 'function') {
+      description = description()
+    }
+    if (Array.isArray(description)) {
+      nodes.push(..._descriptionsToNodes(description))
+    } else {
+      const node = _descriptionToNode(description)
+      if (Array.isArray(node)) {
+        nodes.push(...node)
+      } else {
+        nodes.push(node)
+      }
+    }
+  })
+  return nodes
+}
+
 export function setChildren (element, descriptions, ignoreMethod = false) {
   if (!element) {
     console.log(descriptions)
   }
-  descriptions = _flattenAndMap(descriptions, description => {
-    if (typeof description === 'function') {
-      description = _flattenAndMap(description(element), _createNode)
-    } else {
-      description = _flattenAndMap(description, _createNode)
-    }
-    return description
-  })
+  const nodes = _descriptionsToNodes(descriptions)
   if (!ignoreMethod && element.setChildren) {
-    element.setChildren(descriptions)
+    element.setChildren(nodes)
   } else {
-    descriptions.forEach((description, index) => {
-      if (!(description instanceof Node)) {
-        console.error(description)
-        throw new Error('unhandled description')
+    nodes.forEach((node, index) => {
+      if (!(node instanceof Node)) {
+        console.error(node)
+        throw new Error('unhandled node')
       }
       const referenceNode = element.childNodes[index]
       if (!referenceNode) {
-        element.appendChild(description)
-      } else if (description !== referenceNode) {
-        element.insertBefore(description, referenceNode)
+        element.appendChild(node)
+      } else if (node !== referenceNode) {
+        element.insertBefore(node, referenceNode)
       }
     })
-    while (element.childNodes.length > descriptions.length) {
+    while (element.childNodes.length > nodes.length) {
       element.removeChild(element.lastChild)
     }
   }
@@ -117,6 +126,9 @@ export function setChildren (element, descriptions, ignoreMethod = false) {
 }
 
 export function render (element, descriptions) {
+  if (!descriptions) {
+    return _descriptionsToNodes(element)
+  }
   function f () {
     return setChildren(element, descriptions)
   }
