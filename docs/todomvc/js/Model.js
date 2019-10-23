071c291f5dc39ab86c6b1b60b14caaa2366a9ee0
David Tudury <david.tudury@gmail.com>
2019-10-23 11:40:23 -0700

    refactor todomvc js into one (normal sized) file

--- docs/todomvc/js/Model.js
+++
@@ -1,8 +0,0 @@
-import { remodel } from '../../lib/index.js'
-const model = window.model = remodel({
-  todos: [
-    { label: 'Taste Javascript', completed: false },
-    { label: 'Buy a unicorn', completed: false }
-  ]
-})
-export default model 
