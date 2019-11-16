7d90073ecdaabc308bcee45bfcb227b5bc833bfa
David Tudury <david.tudury@gmail.com>
2019-11-16 08:41:22 -0800

    minor css improvement

--- docs/jsvgx/jsvgx.js
+++ docs/jsvgx/jsvgx.js
@@ -66,7 +66,7 @@ function renderAttributes (attributes, isTopLevel) {
 
 render(document.body, h`
 <header class="app">
-  <svg class="hamburger" onclick="" focusable="false" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <svg class="hamburger" focusable="false" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
     <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
   </svg>
   <h1>
