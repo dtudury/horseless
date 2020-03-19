20b12b460bb923c13027161c9ce5fd865b55cf52
David Tudury <david.tudury@gmail.com>
2020-03-18 22:52:01 -0700

    starting testing

--- docs/test/_mocha-setup.js
+++ docs/test/_mocha-setup.js
@@ -37,4 +37,4 @@ function runTests () {
   })
 }
 
-global.runTests = runTests
+window.runTests = runTests
--- docs/test/index.test.js
+++ docs/test/index.test.js
@@ -1,17 +1,28 @@
-/* global describe it */
+/* global describe it chai */
 
-import { h, render } from '/unpkg/horseless/horseless.js'
+// import { assert } from '/unpkg/chai/chai.js'
+import { h, render, proxy, after } from '/unpkg/horseless/horseless.js'
+
+const assert = chai.assert
 
 const sandbox = document.querySelector('#sandbox')
 
-it('basic rendering', function () {
-})
+function afterUpdate () {
+  return new Promise(resolve => {
+    after(resolve)
+  })
+}
 
-describe('nest', function () {
-  describe('again', function () {
-    this.timeout(5000)
-    it('sandbox', function () {
-      render(sandbox, h`test<span b="a">asdf</span>`)
-    })
+describe('horseless', function () {
+  this.timeout(5000)
+  it('sandbox', async function () {
+    const p = proxy({ a: 'a' })
+    render(sandbox, h`test<span id="attribute-test" a="${() => p.a}">asdf</span>`)
+    const a = document.querySelector('#attribute-test').attributes.a
+    assert.equal(a.value, 'a')
+    p.a = 'b'
+    assert.equal(a.value, 'a')
+    await afterUpdate()
+    assert.equal(a.value, 'b')
   })
 })
