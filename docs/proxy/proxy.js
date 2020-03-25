f30ec8c0318afd7007dd694ab1cdb1dda62622f0
David Tudury <david.tudury@gmail.com>
2020-03-24 18:47:19 -0700

    few more docs examples

--- docs/proxy/proxy.js
+++ docs/proxy/proxy.js
@@ -1,14 +1,14 @@
-import { h, render, remodel } from '../lib/index.js'
+import { h, render, proxy } from '/unpkg/horseless/horseless.js'
 
-const model = remodel({ seconds: 0 })
+const model = proxy({ seconds: 0 })
 setInterval(() => model.seconds++, 1000)
 
 render(document.querySelector('.count'), h`
   <span>seconds running: ${() => model.seconds.toString()}</span>
 `)
 
-// this renders remodel.js into the html pre tag and isn't really part of the example
+// this renders proxy.js into the html pre tag and isn't really part of the example
 // ----------------------------------------------------------------------------------
-window.fetch('remodel.js').then(res => res.text()).then(text => {
+window.fetch('proxy.js').then(res => res.text()).then(text => {
   render(document.querySelector('pre'), h`${text}`)
 })
