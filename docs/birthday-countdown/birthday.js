67332357ca14911b31de7e733bc9cad08529d633
David Tudury <david.tudury@gmail.com>
2020-05-25 13:58:52 -0700

    remove hello world code

--- docs/birthday-countdown/birthday.js
+++ docs/birthday-countdown/birthday.js
@@ -1,8 +1,7 @@
 import { h, render, proxy } from '/unpkg/horseless/horseless.js'
 
-const model = proxy({
-  timeLeft: 0
-})
+const model = proxy({})
+
 const birthday = new Date('August 31 ' + new Date().getFullYear())
 
 setInterval(() => {
@@ -14,10 +13,5 @@ setInterval(() => {
   model.hours = dt % 24
   model.days = Math.floor(dt / 24)
 })
-
-const astronaut = 'Dave'
-const obj = { feeling: 'not sorry' }
-const descriptions = h`I'm <custom-element actually=${obj}>sorry</custom-element> ${astronaut}, I'm afraid I can't do that`
-console.log(JSON.stringify(descriptions, null, '  '))
 
 render(document.body, h`${() => model.days} days, ${() => model.hours} hours, ${() => model.minutes} minutes, ${() => model.seconds} seconds until Izzy's birthday`)
