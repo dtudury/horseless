6a1f1d0ed45862db5032a5fb5439d9007aab2165
David Tudury <david.tudury@gmail.com>
2020-05-26 12:45:51 -0700

    pass model to component

--- docs/tag-component/tag-component.js
+++ docs/tag-component/tag-component.js
@@ -1,9 +1,15 @@
-import { h, render } from '/unpkg/horseless/horseless.js'
+import { h, render, proxy } from '/unpkg/horseless/horseless.js'
+
+const model = proxy({ seconds: 0 })
+setInterval(() => { ++model.seconds }, 1000)
 
 function component (attributes, children) {
+  console.log(attributes.model)
   return h`
     <div>
       attribute a is: ${attributes.a}
+      <br>
+      model.seconds is: ${() => model.seconds}
       <br>
       here's a div with copied attributes: <div ${attributes}></div>
       here's the copied children:
@@ -14,7 +20,7 @@ function component (attributes, children) {
 }
 
 render(document.body, h`
-  <${component} a="b" style="width: 100px; height: 100px; background: red;">
+  <${component} a="b" style="width: 100px; height: 100px; background: red;" model=${model}>
     c
     <br>
     d
