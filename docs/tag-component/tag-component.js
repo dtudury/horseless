69a771617281f6112b1c8ba79c46059d475bb574
David Tudury <david.tudury@gmail.com>
2020-05-26 16:01:05 -0700

    xmlns fix and example to reuse same elements

--- docs/tag-component/tag-component.js
+++ docs/tag-component/tag-component.js
@@ -3,19 +3,22 @@ import { h, render, proxy } from '/unpkg/horseless/horseless.js'
 const model = proxy({ seconds: 0 })
 setInterval(() => { ++model.seconds }, 1000)
 
-function component (attributes, children) {
-  return h`
-    <div>
+const map = new Map()
+
+function component (attributes, children, description) {
+  if (!map.has(description)) {
+    map.set(description, h`
       attribute a is: ${attributes.a}
-      <br>
-      model.seconds is: ${() => model.seconds}
+      <br r=${Math.random() /* just to prove it's reusing the same element */}>
+      model.seconds is: ${() => attributes.model.seconds}
       <br>
       here's a div with copied attributes: <div ${attributes}></div>
       here's the copied children:
       <br>
       ${children}
-    </div>
-  `
+    `)
+  }
+  return map.get(description)
 }
 
 render(document.body, h`
