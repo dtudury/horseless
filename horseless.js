9145a47b2991900ff04cff5c60147f26db108d86
David Tudury <dt@reduct.video>
2021-03-23 23:27:17 -0700

    gently object transformation (instead of assignment)

--- horseless.js
+++ horseless.js
@@ -1,4 +1,10 @@
-export { h } from './lib/decoder.js'
-export { proxy, watchFunction, unwatchFunction, after } from './lib/remodel.js'
-export { render } from './lib/domBuilder.js'
-export { showIfElse, mapEntries, mapSwitch, objToDeclarations } from './lib/mappers.js'
+export { h } from "./lib/decoder.js";
+export { proxy, watchFunction, unwatchFunction, after } from "./lib/remodel.js";
+export { render } from "./lib/domBuilder.js";
+export { copyGently } from "./lib/copier.js";
+export {
+  showIfElse,
+  mapEntries,
+  mapSwitch,
+  objToDeclarations
+} from "./lib/mappers.js";
