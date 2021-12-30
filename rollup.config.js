deb34a1435b74aa11ce995b9249b05056546ea7e
dtudury <david.tudury@gmail.com>
2021-12-29 19:36:09 -0800

    handle null

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,26 @@
 import pkg from './package.json'
 import { terser } from 'rollup-plugin-terser'
-import alias from '@rollup/plugin-alias'
 
-export default [{
-  input: 'horseless.js',
-  inlineDynamicImports: true,
-  plugins: [alias({ entries: [{ find: '/unpkg', replacement: './node_modules' }] }), terser()],
-  output: [
-    { file: `dist/horseless.${pkg.version}.min.esm.js`, format: 'es', sourcemap: true },
-    { file: `dist/${pkg.version}.min.esm.js`, format: 'es', sourcemap: true },
-    { file: pkg.module, format: 'es' }
-  ]
-}, {
-  input: 'horseless.js',
-  plugins: [alias({ entries: [{ find: '/unpkg', replacement: './node_modules' }] })],
-  output: [
-    { file: pkg.main, format: 'cjs' },
-    { file: pkg.devmodule, format: 'es' }
-  ]
-}]
+export default [
+  {
+    input: 'horseless.js',
+    inlineDynamicImports: true,
+    plugins: [terser()],
+    output: [
+      {
+        file: `dist/horseless.${pkg.version}.min.esm.js`,
+        format: 'es',
+        sourcemap: true
+      },
+      { file: `dist/${pkg.version}.min.esm.js`, format: 'es', sourcemap: true },
+      { file: pkg.module, format: 'es' }
+    ]
+  },
+  {
+    input: 'horseless.js',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.devmodule, format: 'es' }
+    ]
+  }
+]
